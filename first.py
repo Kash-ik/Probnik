dcb403b757cf096271869c31dfc152b651c0709e
Kash-ik <Kolesnikovaalina.dom@mail.ru>
2026-05-14 13:25:19 +0400

    Итеративный поиск n-ого числа Фибоначчи

--- first.py
+++ first.py
@@ -1,8 +1,12 @@
 def fib(a):
-    if a < 3:
-        if a <= 1:
-            return 0
-        if a == 2:
-            return 1
+    if a <= 1:
+        return 0
+    if a == 2:
+        return 1
     return fib(a - 1) + fib(a-2)
+    b,c = 0, 1
+    for x in range(a):
+        print(b)
+        b,c = c,b+c
+    return ""
 print(fib(10))
